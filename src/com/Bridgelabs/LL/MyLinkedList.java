dd2f05906d64c17a3a5a2384621d9e669c3ae9d5
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-24 23:59:02 +0530

    [pritesh] Added elements to LL

--- src/com/Bridgelabs/LL/MyLinkedList.java
+++ src/com/Bridgelabs/LL/MyLinkedList.java
@@ -5,6 +5,33 @@ public class MyLinkedList {
 	private INode head;
 	private INode tail;
 	
+	public void add(INode myNode) {
+		if (tail == null) {
+			tail = myNode;
+		}
+		if (head == null) {
+			head = myNode;
+		} else {
+			INode tempNode = head;
+			head = myNode;
+			head.setNext(tempNode);
+		}
+
+	}
+	
+	public void print() {
+		INode tempNode = head;
+		while (tempNode != null) {
+			System.out.print(tempNode.getkey());
+			if (!tempNode.equals(tail)) {
+				System.out.print("->");
+			}
+			tempNode = tempNode.getNext();
+			
+		}
+
+	}
+	
 
 }
 
--- src/com/Bridgelabs/LL/MyNodeRunner.java
+++ src/com/Bridgelabs/LL/MyNodeRunner.java
@@ -2,13 +2,16 @@ package com.Bridgelabs.LL;
 
 public class MyNodeRunner {
 	public static void main(String[] args) {
-		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
 		MyNode<Integer> secNode = new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
-		firstNode.setNext(secNode);
-		secNode.setNext(thirdNode);
-		boolean result = firstNode.getNext().equals(secNode) && firstNode.getNext().getNext().equals(thirdNode);
-		System.out.println(result);
+		MyNode<Integer> firstNode = new MyNode<Integer>(70);
+		MyLinkedList linkedList = new MyLinkedList();
+		linkedList.add(firstNode);
+		linkedList.add(secNode);
+		linkedList.add(thirdNode);
+		
+		linkedList.print();
 
 	}
 }
