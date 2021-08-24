c4aa5eaee2b92cfe9514901237fd61d8538c1872
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-24 23:54:38 +0530

    [pritesh] Created simple LinkList

--- src/com/Bridgelabs/LL/MyNodeRunner.java
+++ src/com/Bridgelabs/LL/MyNodeRunner.java
@@ -2,7 +2,13 @@ package com.Bridgelabs.LL;
 
 public class MyNodeRunner {
 	public static void main(String[] args) {
-
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		firstNode.setNext(secNode);
+		secNode.setNext(thirdNode);
+		boolean result = firstNode.getNext().equals(secNode) && firstNode.getNext().getNext().equals(thirdNode);
+		System.out.println(result);
 
 	}
 }
